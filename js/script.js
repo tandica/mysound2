c5baa31d3c554203619c5e9e9be1757d2579d9bf
tandica <williams.tandica@gmail.com>
2022-09-01 19:26:45 -0400

    step 5 - add the rest of the logic to allow for the songs to show up on the page

--- js/script.js
+++ js/script.js
@@ -17,8 +17,28 @@ const total = document.querySelector(".text");
 
 //create remove/hide functionality when clicking the button
 button.addEventListener("click", function () {
+  mySongs(myMusicList);
   //remove "hide" class from ul list
   mixList.classList.remove("hide");
   //add "hide" class to the button element
   button.classList.add("hide");
 });
+
+//create subheading text that displays the number of songs
+total.innerText = `My current top ${myMusicList.length} songs 🎧`;
+
+//create a function that turns the array of strings into html list elements to display on the page
+const mySongs = function (songs) {
+  //forEach() executes a provided function once for each array element
+  //this is how we are able to access each string in the list we made
+  songs.forEach(function (song, index) {
+    //create a list element
+    let li = document.createElement("li");
+    //add a class to the list element named "song"
+    li.classList.add("song");
+    //create html code which has the song number and name of the song
+    li.innerHTML = `<span class = "song-number">#${index + 1}</span> ${song}`;
+    //add each song to the list element
+    mixList.append(li);
+  });
+};
